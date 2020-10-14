d80a1d03d755e35048c9b4da70304c27d4c7f8a3
ZoomIsland <zacherymilan@gmail.com>
2020-10-14 16:52:33 -0600

    Tried async in action -- doesn't work

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,7 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from '../reducers/index';
+import { forbiddenWordsMiddleware } from "../middleware";
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  applyMiddleware(forbiddenWordsMiddleware));
 // store can also take initial state to do state preloading or server side rendering
 
 export default store;
