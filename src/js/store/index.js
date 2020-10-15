4155dd62cac21bea04e916e71f4922f883acb46b
ZoomIsland <zacherymilan@gmail.com>
2020-10-15 11:20:08 -0600

    Saga begun, needs work

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,14 +1,20 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from '../reducers/index';
 import { forbiddenWordsMiddleware } from "../middleware";
-import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import apiSaga from "../sagas/api-saga";
+
+const initializeSagaMiddleWare = createSagaMiddleware();
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
+  storeEnhancers(
+    applyMiddleware(forbiddenWordsMiddleware, initializeSagaMiddleWare))
 );
+
+initializeSagaMiddleWare.run(apiSaga);
 // store can also take initial state to do state preloading or server side rendering
 
 export default store;
